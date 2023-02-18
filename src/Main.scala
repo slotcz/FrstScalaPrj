b64a8fe1d70c9cbffbdc4edfb958d1d7aad914dc
Sergey Lozovoy <lisovice.cz@outlook.com>
2023-02-18 16:26:28 +0100

    commit after changed, add comments

--- src/Main.scala
+++ src/Main.scala
@@ -2,7 +2,7 @@
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
-    val pnt = new Point(10, 10);
-    pnt.move(20, 20);
+    val pnt = new Point(10, 10); // create instance object point
+    pnt.move(20, 20); // move point
   }
 }
--- src/Point.scala
+++ src/Point.scala
@@ -1,9 +1,10 @@
 import java.io._
-
+// it is class point, init and move
 class Point(xc: Int, yc: Int) {
   private var x: Int = xc
   private var y: Int = yc
 
+  // comment for method move point
   def move(dx: Int, dy: Int): Unit = {
     x = x + dx
     y = y + dy
